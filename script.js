e0965a1256ee5a01fdf4f92a80a910a725591bfc
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-12-14 22:42:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+function appendValue(value) {
+    document.getElementById('input').value += value;
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById('input').value);
+        document.getElementById('input').value = result;
+    } catch (error) {
+        if (error instanceof SyntaxError) {
+            document.getElementById('input').value = 'Error';
+        }
+    }
+}
+
+function clearInput() {
+    document.getElementById('input').value = '';
+}
